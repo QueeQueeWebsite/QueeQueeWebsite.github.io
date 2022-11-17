cc8634a0ac4f85c71adb76154514801ee2979adf
Quinton <q@gmail.com>
2022-11-17 15:33:23 -0700

    trying to reference json file

--- scripts/discord_message.js
+++ scripts/discord_message.js
@@ -1,11 +1,12 @@
-
+import config from 'config.json' assert { type: 'JSON'};
 
 function initialize() {
     $('#Submit').click(send_message_to_server);
 }
 
 function send_message_to_server(message = "test_message"){
-    var url = $("#url").val();
+    var url = config.url;
+    console.log(url)
     var content = $("#content").val(); 
     $.post(url, { "content": content }, function () {
 					document.getElementById('wb-text').innerHTML = "Message Sent to Discord.";
