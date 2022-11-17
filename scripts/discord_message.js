cc9b2d9925d6ca72621e72be5d25776d0ed8c4ae
Quinton <q@gmail.com>
2022-11-17 16:16:32 -0700

    added searchparam grabbing

--- scripts/discord_message.js
+++ scripts/discord_message.js
@@ -2,11 +2,12 @@ import config from 'config.json' assert { type: 'JSON'};
 
 function initialize() {
     $('#Submit').click(send_message_to_server);
+    const params = new Proxy(new URLSearchParams(window.location.search), {get: (searchParams, prop) => searchParams.get(prop),});
+    console.log(params);
 }
 
 function send_message_to_server(message = "test_message"){
     var url = config.url;
-    console.log(url)
     var content = $("#content").val(); 
     $.post(url, { "content": content }, function () {
 					document.getElementById('wb-text').innerHTML = "Message Sent to Discord.";
