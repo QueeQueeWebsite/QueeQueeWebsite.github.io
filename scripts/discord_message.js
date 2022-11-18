65dd660c85bc733f89daa612e041e2c5004997d6
Quinton <q@gmail.com>
2022-11-17 17:07:54 -0700

    import changed to fetch

--- scripts/discord_message.js
+++ scripts/discord_message.js
@@ -1,5 +1,3 @@
-import config from './config.json' assert { type: 'JSON' };
-
 function initialize() {
     $('#Submit').click(send_message_to_server);
     const params = new Proxy(new URLSearchParams(window.location.search), {get: (searchParams, prop) => searchParams.get(prop),});
@@ -7,7 +5,10 @@ function initialize() {
 }
 
 function send_message_to_server(message = "test_message"){
-    var url = config.url;
+    var url = fetch('./config.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
     var content = $("#content").val(); 
     $.post(url, { "content": content }, function () {
 					document.getElementById('wb-text').innerHTML = "Message Sent to Discord.";
