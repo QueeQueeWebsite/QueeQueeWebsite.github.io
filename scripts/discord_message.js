358c1b81ef53b2aac52dcb72911ea6a4d3ca0064
Quinton <q@gmail.com>
2022-12-22 14:57:22 -0700

    trying to fix message sending from parameters

--- scripts/discord_message.js
+++ scripts/discord_message.js
@@ -1,5 +1,5 @@
 function initialize() {
-    $('#Submit').click(send_message_to_server);
+    $('#Submit').click(() => {send_message_to_server()});
     const params = new Proxy(new URLSearchParams(window.location.search), {get: (searchParams, prop) => searchParams.get(prop),});
     console.log(params.name);
 }
@@ -10,6 +10,7 @@ async function get_url(){
 async function post_message(message){
     let url = await get_url()
     //var content = message;//$("#content").val(); 
+    console.log(message);
     $.post(url, { "content": message });
 }
 function send_message_to_server(message = "test_message"){
