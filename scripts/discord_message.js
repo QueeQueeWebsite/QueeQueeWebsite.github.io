6d7a26d45e4bae704129916ce4186693e1d26319
Quinton <q@gmail.com>
2022-12-22 14:29:39 -0700

    can now grab url from config file

--- scripts/discord_message.js
+++ scripts/discord_message.js
@@ -3,27 +3,23 @@ function initialize() {
     const params = new Proxy(new URLSearchParams(window.location.search), {get: (searchParams, prop) => searchParams.get(prop),});
     console.log(params.name);
 }
-async function get_url_helper(){
+async function get_url(){
 	return fetch('./scripts/config.json')
     .then((response) => response.json()).then((result) => result.url);
 }
-async function get_url(){
-	let url = await get_url_helper();
-    console.log(url);
-}
-async function post_message(){
-
+async function post_message(message){
+    let url = await get_url()
+    var content = $("#content").val(); 
+    $.post(url, { "content": content }, function () {
+					document.getElementById('wb-text').innerHTML = "Message Sent to Discord.";
+					document.getElementById("form").style.display = "none";
+					setTimeout(() => document.location.reload(), 5000);
+                });
 }
 function send_message_to_server(message = "test_message"){
     /*var url = fetch('./scripts/config.json')
     .then((response) => {return response.json();})
     console.log(url);*/
-    var url = get_url();
+    post_message(message);
 
-    //var content = $("#content").val(); 
-    /*$.post(url, { "content": content }, function () {
-					document.getElementById('wb-text').innerHTML = "Message Sent to Discord.";
-					document.getElementById("form").style.display = "none";
-					setTimeout(() => document.location.reload(), 5000);
-                });*/
 }
