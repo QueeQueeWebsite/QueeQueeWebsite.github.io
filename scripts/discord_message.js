405d59a5ad83dab297d9b1b470ca2679f00f9ae8
Q <>
2023-01-08 17:07:37 -0700

    trying to work with oauth2

--- scripts/discord_message.js
+++ scripts/discord_message.js
@@ -1,9 +1,34 @@
 let props = {}; // empty object to hold all props, gets initialized by initialize()
+function exchange_code(){
+    console.log('exchange called');
+    data = {
+        'client_id': '946542952946216960',
+        'client_secret': 'S12fC9TFq7dqfKZ5HOOfL714pBRa2Wm7',
+        'grant_type': 'authorization_code',
+        'code': props.code,
+        'redirect_uri': 'https://queequeewebsite.github.io'
+    }
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    fetch('https://discord.com/api/v10', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: JSON.stringify(data)
+    })
+    .then(response => console.log(response.json()));
+}
+
 function initialize() {
     //$('#Submit').click(() => {send_message_to_server()});
     $('#Submit').click(() => {get_current_balance()});
     let params = new Proxy(new URLSearchParams(window.location.search), {get: (searchParams, prop) => searchParams.get(prop),});
     props = params;
+    exchange_code();
+    let sidebar = document.getElementById("sidebar");
+    let new_link = document.createElement("a");
+    new_link.innerHTML = "special_generated_link";
+    sidebar.appendChild(new_link);
 }
 async function get_url(){
 	return fetch('./scripts/config.json')
