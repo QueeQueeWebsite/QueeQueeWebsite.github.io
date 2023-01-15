dd9402b89c66d8ff4579e1a740fb4857c344be4b
Q <>
2023-01-14 21:17:52 -0700

    got rid of console.logs, added an avatar lookup for user

--- scripts/default_initializer.js
+++ scripts/default_initializer.js
@@ -3,13 +3,12 @@
 props = {}
 
 function update_cookies_user(user_data){
-    let cookie_addon = ";SameSite=None" + ";secure";
-    console.log('cookie_update + ');
     console.log(user_data);
-    console.log('cookie: ' + document.cookie);
+    let cookie_addon = ";SameSite=None" + ";secure";
     document.cookie = "username=" + user_data.username + cookie_addon;
     document.cookie = "id=" + user_data.id + cookie_addon;
     document.cookie = "discriminator=" + user_data.discriminator + cookie_addon;
+    document.cookie = "avatar_link=" + user_data.avatar + cookie_addon;
 }
 function getCookie(name) {
   const value = `; ${document.cookie}`;
@@ -19,10 +18,10 @@ function getCookie(name) {
 function get_cookie_user(){
     const return_user = {};
     const cookies = document.cookie;
-    console.log('cookies from "get_cookie_user()" ' + cookies);
     return_user.username = getCookie('username');
     return_user.id = getCookie('id');
     return_user.discriminator = getCookie('discriminator');
+    return_user.avatar_link = getCookie('avatar_link');
     return return_user;
 }
 function is_user_logged(){
@@ -31,8 +30,6 @@ function is_user_logged(){
 }
 function exchange_code(){
     const url = "https://discord.com/api/v10";
-    console.log('exchange called');
-    console.log(props.code);
     const params = new URLSearchParams();
     params.append('client_id','946542952946216960');
     params.append('client_secret', 'S12fC9TFq7dqfKZ5HOOfL714pBRa2Wm7');
@@ -60,21 +57,29 @@ function exchange_code(){
     })
     .then(response => response.json()).then(data => fetch(url + '/users/@me', { method: 'GET', headers: {'Authorization': 'Bearer ' + data.access_token}}).then(response => response.json()).then(data=>update_cookies_user(data)));
 }
+function strip_href(ref){
+    if(ref != undefined){
+        let splitter = ref.split('/');
+        return splitter[splitter.length - 1];
+    }
+}
 function sidebar_initialize(){
     var path = window.location.pathname;
-    var page = path.split("/").pop();
+    var current_page = path.split("/").pop();
     let page_names = ['Profile','QueeQueeClicker','Stocks']
-    let hrefs = ['index.html','pages/qqclicker.html']
-    console.log( page );
+    let hrefs = ['index.html','pages/qqclicker.html','pages/stocks.html']
     let sidebar = document.getElementById("sidebar");
     for (page in page_names) {
         let new_link = document.createElement("a");
         new_link.innerHTML = page_names[page];
         sidebar.appendChild(new_link);
         let href = hrefs[page];
-        if (page == 'index.html'){
+        if (current_page != 'index.html'){
             href = '../' + href;
-            new_link.class = 'active';
+            //new_link.class = 'active';
+        }
+        if(current_page == strip_href(href)){
+            new_link.classList.add('active');
         }
         new_link.href = href;
     }
@@ -82,8 +87,6 @@ function sidebar_initialize(){
 
 function default_initialize(){
     if (window.opener && window.opener !== window) {
-        // you are in a popup so do nothing
-        console.log('popup?');
         return
     }
     let params = new Proxy(new URLSearchParams(window.location.search), {get: (searchParams, prop) => searchParams.get(prop),});
@@ -103,13 +106,10 @@ function default_initialize(){
             console.log('user is logged');
         }
         else{ // if not logged in send them to login page
-            console.log('should show login page');
             let current_url = location.href;
             let new_url = current_url.split('/')[0];
-            console.log(new_url);
             location.href = new_url + '/pages/login.html';
         }
     }
-    console.log(get_cookie_user());
     sidebar_initialize();
 }
